d2ac597a7486baa2c78a9bdeab341a642fbe9281
Raul Afonso <afonsoafn@gmail.com>
2025-05-28 12:09:35 +0100

    feat: add initial failing unit tests for api

---
+++ app/api/products.ts
@@ -0,0 +1,11 @@
+import { Product } from "../features/products/types";
+
+let mockProducts: Product[] = [];
+
+export const fetchProducts = async (): Promise<any> => {};
+
+export const createProduct = async (product: Product): Promise<any> => {};
+
+export const updateProduct = async (updated: Product): Promise<any> => {};
+
+export const deleteProduct = async (id: string): Promise<any> => {};
