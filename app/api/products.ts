ff3dc15c964af55ba01071915c20d6291ff6accc
Raul Afonso <afonsoafn@gmail.com>
2025-05-28 12:11:11 +0100

    feat: implement mock api

--- app/api/products.ts
+++ app/api/products.ts
@@ -2,10 +2,29 @@ import { Product } from "../features/products/types";
 
 let mockProducts: Product[] = [];
 
-export const fetchProducts = async (): Promise<any> => {};
+export const fetchProducts = async (): Promise<Product[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve([...mockProducts]), 200);
+  });
+};
 
-export const createProduct = async (product: Product): Promise<any> => {};
+export const createProduct = async (product: Product): Promise<Product> => {
+  mockProducts.push(product);
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(product), 200);
+  });
+};
 
-export const updateProduct = async (updated: Product): Promise<any> => {};
+export const updateProduct = async (updated: Product): Promise<Product> => {
+  mockProducts = mockProducts.map((p) => (p.id === updated.id ? updated : p));
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(updated), 200);
+  });
+};
 
-export const deleteProduct = async (id: string): Promise<any> => {};
+export const deleteProduct = async (id: string): Promise<string> => {
+  mockProducts = mockProducts.filter((p) => p.id !== id);
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(id), 200);
+  });
+};
