679d7a2efea10b92e3b7a38942fbcb1f4e558380
Raul Afonso <afonsoafn@gmail.com>
2025-05-30 15:34:47 +0100

    feat: implement api calls

--- app/api/products.ts
+++ app/api/products.ts
@@ -1,30 +1,31 @@
-import { Product } from "../features/products/types";
+import { CreateProduct, Product } from "@/features/products/types";
 
-let mockProducts: Product[] = [];
+const API_URL = "http://localhost:4000/api/products";
 
-export const fetchProducts = async (): Promise<Product[]> => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve([...mockProducts]), 200);
-  });
+export const fetchProducts = async () => {
+  const res = await fetch(API_URL);
+  return res.json();
 };
 
-export const createProduct = async (product: Product): Promise<Product> => {
-  mockProducts.push(product);
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(product), 200);
+export const createProduct = async (product: CreateProduct) => {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(product),
   });
+  return res.json();
 };
 
-export const updateProduct = async (updated: Product): Promise<Product> => {
-  mockProducts = mockProducts.map((p) => (p.id === updated.id ? updated : p));
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(updated), 200);
+export const updateProduct = async (product: Product) => {
+  const res = await fetch(`${API_URL}/${product.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(product),
   });
+  return res.json();
 };
 
-export const deleteProduct = async (id: string): Promise<string> => {
-  mockProducts = mockProducts.filter((p) => p.id !== id);
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(id), 200);
-  });
+export const deleteProductById = async (id: string) => {
+  await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+  return id;
 };
